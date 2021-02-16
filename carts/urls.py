431a50291c5adfa553faac68d9745ae933f5ed99
Purusoth Shanmugarajah <shanmugarajahpurusoth@gmail.com>
2021-02-16 11:29:44 +0530

    mail

--- carts/urls.py
+++ carts/urls.py
@@ -4,9 +4,10 @@ from . import views
 
 urlpatterns = [
     path('',views.view,name='cart'),
-#    path('<int:pk>/',views.update_cart,name='update_cart'),
- #   path('post/<int:pk>/', PostDetailView.as_view(),name='food-detail'),
-     path('<int:id>/', views.update_cart, name='update_cart'),
+    path('mail/',views.mail,name='mail'),
+#   path('<int:pk>/',views.update_cart,name='update_cart'),
+ #  path('post/<int:pk>/', PostDetailView.as_view(),name='food-detail'),
+    path('<int:id>/', views.update_cart, name='update_cart'),
      
 
 
--- kilobiryani/settings.py
+++ kilobiryani/settings.py
@@ -137,3 +137,10 @@ CRISPY_TEMPLATE_PACK='bootstrap4'
 LOGIN_REDIRECT_URL = 'index'
 
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'mammamaathiyosi@gmail.com'
+EMAIL_HOST_PASSWORD = 'Jaffna2019'
