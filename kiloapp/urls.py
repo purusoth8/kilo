eaca9aa2f4c3aa031f9ea12561829f35916e1ac8
Purusoth <purusoth.shanmugaragah@live.vu.edu.au>
2021-02-18 13:40:59 +0530

    more change

--- kiloapp/urls.py
+++ kiloapp/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('menu/',views.menu,name='menu'),
     path('contact/',views.contact,name='contact'),
-    path('blog/',views.blog,name='blog'),
     path('about/',views.about,name='about'),
     path('post/<int:pk>/', PostDetailView.as_view(),name='food-detail'),
 
